f926448efe558e18ec8352f910424c0251b348fd
Aimar Anand <aimaranand02@gmail.com>
2021-10-02 01:32:06 +0800

    debugging

--- basic-algorithm/greedyAlgorithm.js
+++ basic-algorithm/greedyAlgorithm.js
@@ -1,9 +1,7 @@
-const mataUang = [
-  100000, 75000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 100,
-];
-const hargaBarang = 9300;
+const mataUang = [1000, 500, 400, 200, 100];
+const hargaBarang = 9800;
 const uangOrang = 50000;
-const kembalian = uangOrang - hargaBarang;
+const kembalian = 1800;
 
 function greedyAlgo(mataUang, kembalian) {
   console.time("tes");
