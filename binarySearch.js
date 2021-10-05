7f7a992a21b5519b1eb8d72ce076bbad52bee872
Aimar Anand <aimaranand02@gmail.com>
2021-10-06 01:34:53 +0800

    menambah iteration loop pada binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -4,6 +4,7 @@ let x = 9;
 
 console.log(binarySearch(0, length, 0, x)); //binarySearch(awal, akhir/length, mid, ygdicari)
 
+//recursive
 function binarySearch(awal, length, mid, x) {
   mid = Math.floor((length + awal) / 2);
   if (arr[mid] == x) {
@@ -18,3 +19,20 @@ function binarySearch(awal, length, mid, x) {
   }
   return binarySearch(awal, length, mid, x);
 }
+
+//iteration loop
+// function binarySearch(awal, length, mid, x) {
+//   while (true) {
+//     mid = Math.floor((length + awal) / 2);
+//     if (arr[mid] == x) {
+//       return mid;
+//     } else if (arr[mid] <= x) {
+//       awal = mid + 1;
+//     } else if (arr[mid] >= x) {
+//       length = mid - 1;
+//     }
+//     if (length < awal) {
+//       return "Data tidak ditemukan";
+//     }
+//   }
+// }
