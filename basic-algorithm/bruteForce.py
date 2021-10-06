bb7b0f549f8b6a7edfcb5cc4fc16d53929e79b26
Aimar Anand <aimaranand02@gmail.com>
2021-10-06 23:04:03 +0800

    modifikasi bruteforce.py

--- basic-algorithm/bruteForce.py
+++ basic-algorithm/bruteForce.py
@@ -1,26 +1,27 @@
-coins = [ 500, 1000, 2000]
+coins = [500, 1000, 2000]
 change = 8000
-hasil = []
+
 
 def coin_change(c):
-  if c == 0:
-    return [[]]     
-  if c < 0:
-    return []      
-  else:
-    all_combos = []
-    for i in coins:
-      recursive_result = coin_change(c-i)
-      for combo in recursive_result:
-        combo.append(i)
-      all_combos.extend(recursive_result)
+    if c == 0:
+        return [[]]
+    if c < 0:
+        return []
+    else:
+        all_combos = []
+        for i in coins:
+            recursive_result = coin_change(c-i)
+            for combo in recursive_result:
+                combo.append(i)
+            all_combos.extend(recursive_result)
 
-    return all_combos
+        return all_combos
+
 
 result = coin_change(change)
 for combo in result:
-  print(combo) 
-  # if(len(combo) >= len(result)):
-  #   continue
-  # hasil = combo
-  # print(hasil)
+    print(combo)
+    # if(len(combo) >= len(result)):
+    #   continue
+    # hasil = combo
+    # print(hasil)
