a5a20e1cbaf4292f3c32cb043b06849eded69883
Aimar Anand <aimaranand02@gmail.com>
2021-10-19 21:51:22 +0800

    mengefisien binary search

--- binarySearch.js
+++ binarySearch.js
@@ -2,16 +2,16 @@ let arr = [1, 3, 5, 7, 9, 12, 13, 17];
 let length = arr.length - 1;
 let x = 9;
 
-console.log(binarySearch(0, length, x)); //binarySearch(awal, akhir/length, mid, ygdicari)
+console.log(binarySearch(0, length, x)); //binarySearch(awal, akhir/length, target)
 
 //recursive
 function binarySearch(awal, length, x) {
   let mid = Math.floor((length + awal) / 2);
   if (arr[mid] == x) {
     return mid;
-  } else if (arr[mid] <= x) {
+  } else if (arr[mid] < x) {
     awal = mid + 1;
-  } else if (arr[mid] >= x) {
+  } else if (arr[mid] > x) {
     length = mid - 1;
   }
   if (length < awal) {
