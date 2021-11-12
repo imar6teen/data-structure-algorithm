96aad7fef5378a8021f36c5de38e2008ed599ee5
Aimar Anand <aimaranand02@gmail.com>
2021-11-12 19:04:26 +0800

    mengubah divide and conquer

--- basic-algorithm/DivideAndConquer.js
+++ basic-algorithm/DivideAndConquer.js
@@ -1,21 +1,32 @@
-const array = [
-  200, 2, 50, 80, 100, 2000, 120, 400, 5, 3, 20, 16, 88, 25, 90, 1092,
-];
+let array = [200, 2, 50, 80, 5, 3, 20];
 let akhir = array.length - 1;
 let mulai = 0;
-const dacMax = (array, mulai, akhir) => {
-  let kiri = 0;
-  let kanan = 0;
+// const dacMax = (array, mulai, akhir) => {
+//   if (akhir - mulai === 1) {
+//     if (array[mulai] >= array[akhir]) return array[mulai];
+//     else return array[akhir];
+//   } else if (akhir - mulai === 0) return array[mulai];
+
+//   let mid = Math.floor((akhir + mulai) / 2);
+//   let kiri = dacMax(array, mulai, mid);
+//   let kanan = dacMax(array, mid + 1, akhir);
+//   if (kiri > kanan) return kiri;
+//   else if (kiri <= kanan) return kanan;
+// };
+
+// console.log(dacMax(array, mulai, akhir));
+
+const dacMin = (array, mulai, akhir) => {
   if (akhir - mulai === 1) {
-    if (array[mulai] >= array[akhir]) return array[mulai];
-    else return array[akhir];
+    if (array[mulai] >= array[akhir]) return array[akhir];
+    else return array[mulai];
   } else if (akhir - mulai === 0) return array[mulai];
 
-  let mid = Math.floor((akhir + mulai) / 2);
-  kiri = dacMax(array, mulai, mid);
-  kanan = dacMax(array, mid + 1, akhir);
-  if (kiri > kanan) return kiri;
-  else if (kiri <= kanan) return kanan;
+  let mid = Math.floor((mulai + akhir) / 2);
+  let kiri = dacMin(array, mulai, mid);
+  let kanan = dacMin(array, mid + 1, akhir);
+  if (kiri > kanan) return kanan;
+  else return kiri;
 };
 
-console.log(dacMax(array, mulai, akhir));
+console.log(dacMin(array, mulai, akhir));
