70435120c629cde82b69980ff8b59bf5a1a71716
Aimar Anand <aimaranand02@gmail.com>
2022-04-28 23:44:18 +0800

    tes ubah commit messagssss

--- basic-algorithm/BubbleSort.js
+++ basic-algorithm/BubbleSort.js
@@ -14,11 +14,11 @@ function bubbleSort(arr) {
   return arr;
 }
 
-let arr = [];
+let arr = [5, 3, 4, 1, 2, 9, 7, 8, 6, 22, 76, 3, 4, 1, 2, 9, 7, 8, 6, 22, 76];
 
-for (let i = 0; i < 5000; i++) {
-  arr[i] = Math.floor(Math.random() * 30);
-}
+// for (let i = 0; i < 5000; i++) {
+//   arr[i] = Math.floor(Math.random() * 30);
+// }
 
 console.time("bubbleSort");
 bubbleSort(arr);
